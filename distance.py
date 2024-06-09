67410a9e2fbf13c24c03f22a36760a7f5fd287d6
chandu <141409100+Chandrakant6@users.noreply.github.com>
2024-06-09 15:53:48 +0530

    changed variable name distance.py
    point -> vector
    vector -> path

--- distance.py
+++ distance.py
@@ -4,12 +4,12 @@ random.seed(0)
 
 # third iteration for any no. of dimensions
 def calc(a: list[float], b: list[float]) -> tuple[list[float],list[float],float]:  
-    point = [abs(i-j) for i, j in zip(a, b)].sort()
+    vector = [abs(i-j) for i, j in zip(a, b)].sort()
     roots = [sqrt(i) for i in range(1,len(a)+1)][::-1]
 
-    vector = [point[0]] + [point[i+1]-point[i] for i in range(len(point)-1)]
+    path = [vector[0]] + [vector[i+1]-vector[i] for i in range(len(vector)-1)]
 
-    return point, vector, sum([i*j for i, j in zip(vector, roots)])
+    return vector, path, sum([i*j for i, j in zip(path, roots)])
 
 # testing
 if __name__ == '__main__':
