817f5da5c1fd257360c3f0642940d98ff694d6c1
chandu <141409100+Chandrakant6@users.noreply.github.com>
2024-06-09 15:31:04 +0530

    adde simpler general equation to distance.py
    third iteration

--- distance.py
+++ distance.py
@@ -24,3 +24,12 @@ def calc_general(a: list[float], b: list[float]):
             p = np.subtract(p[1:], p[0])
             roots.pop(0)
     return dist
+
+# third iteration for any no. of dimensions
+def calc_gen2(a: list[float], b: list[float]):  
+    point = [abs(i-j) for i, j in zip(a, b)].sort()
+    roots = [sqrt(i) for i in range(1,len(a)+1)][::-1]
+
+    vector = [point[0]] + [point[i+1]-point[i] for i in range(len(point)-1)]
+
+    return point, vector, sum([i*j for i, j in zip(vector, roots)])
